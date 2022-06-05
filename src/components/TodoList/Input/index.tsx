6aac90ae6dd6f68f9134933775232ddf33539f85
Chengyf <1351416566@qq.com>
2022-06-05 16:57:57 +0800

    feat: 初始化项目

---
+++ src/components/TodoList/Input/index.tsx
@@ -0,0 +1,41 @@
+import { FC, ReactElement, useRef } from "react";
+import { ITodo } from "../interfaces";
+
+interface IProps {
+    addTodo: (todo: ITodo) => void;
+    todoList: ITodo[];
+}
+
+const Input: FC<IProps> = ({
+    addTodo,
+    todoList,
+}): ReactElement => {
+
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    const addItem = (): void => {
+        const val: string = inputRef.current!.value.trim();
+        if (val.length) {
+            const isExist = todoList.find(todo => todo.content === val);
+            if (isExist) {
+                alert('已存在该项');
+                return;
+            }
+            addTodo({
+                id: new Date().getTime(),
+                content: val,
+                completed: false
+            })
+            inputRef.current!.value = '';
+        }
+    }
+
+    return (
+        <div>
+            <input type="text" placeholder="请输入待办项" ref={inputRef}></input>
+            <button onClick={addItem}>增加</button>
+        </div>
+    )
+}
+
+export default Input;
---
+++ src/components/TodoList/index.tsx
@@ -0,0 +1,9 @@
+function TodoList() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default TodoList;
